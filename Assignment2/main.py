bf601d67da9cf00d3feb32a3f5cdabc8a724e721
Cole <colekoryto17@gmail.com>
2025-10-01 11:48:37 -0500

    Added reporting prints

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -16,17 +16,17 @@ def main():
     vol_strategy = VolatilityBreakoutStrategy()
     
     
-    # strategies = (benchmark_strategy, ma_strategy, vol_strategy, macd_strategy, rsi_strategy)
-    strategies = [ma_strategy]
+    strategies = (benchmark_strategy, ma_strategy, vol_strategy, macd_strategy, rsi_strategy)
     new_sim = MarketSimulation(1_000_000, strategies, symbols=["AAPL"])
 
     new_sim.run_simulation()
 
     reporter = Reporting(new_sim.NAV_series)
     print(f"Final Cash Balance = ${new_sim.cash_balance:,.2f}")
+    print(f"Final NAV = ${new_sim.NAV_series.iloc[-1]:,.2f}")
     print(f"P&L = ${reporter.compute_pnl():,.2f}")
     print(f"Total Return: {reporter.compute_total_return():,.2%}")
-    print(f"Sharpe Ratio: {reporter.sharpe_ratio():,.2}")
+    print(f"Sharpe Ratio: {reporter.sharpe_ratio():,.2f}")
     print(f"Max Drawdown: {reporter.max_drawdown():,.2%}")
     reporter.plot_equity_curve()  
 
