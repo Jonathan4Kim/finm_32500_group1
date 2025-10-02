c0f7d7b14d1572b28ccdecc22d176dcaa1b5baf5
Jonathan Kim <112917210+Jonathan4Kim@users.noreply.github.com>
2025-10-01 19:44:21 -0500

    strategies finished impl

--- Assignment2/rsi_strategy.py
+++ Assignment2/rsi_strategy.py
@@ -5,53 +5,63 @@ from collections import deque
 class RSIStrategy(Strategy):
     def __init__(self):
         self.__total_gain = {}
-        self.__num_gain = {}
         self.__total_loss = {}
-        self.__num_loss = {}
         self.__prev_price = {}
         self.__dq = {}
+
 
     def generate_signals(self, tick):
         # get attributes from MarketDataPoint
         price = tick.price
         symbol = tick.symbol
-        # calculate price change
-        if self.__dq.get(symbol, deque([])):
+        # calculate price change. There's probably a way to make this more efficient
+        if symbol not in self.__dq:
+            self.__dq[symbol] = deque([])
+            self.__total_gain[symbol] = 0.0
+            self.__total_loss[symbol] = 0.0
             self.__prev_price[symbol] = price
-            price_change = price - self.__prev_price.get(symbol, 0.0)
-        else:
             return ["HOLD"]
+        
+        # calculate price change and update previous price
+        price_change = price - self.__prev_price[symbol]
+        self.__prev_price[symbol] = price
         # add value to total gain/loss depending on difference
         if price_change < 0:
             # add value to losses
-            self.__total_loss[symbol] = self.__total_loss.get(symbol, 0.0) + abs(price_change)
-            self.__num_loss[symbol] = self.__num_loss.get(symbol, 0) + 1
+            self.__total_loss[symbol] = self.__total_loss[symbol] + abs(price_change)
         elif price_change > 0:
-            self.__total_gain[symbol] = self.__total_gain.get(symbol, 0.0) + price_change
-            self.__num_gain[symbol] = self.__num_gain.get(symbol, 0) + 1
-        # keep previous value and add new price change to deque
-        self.__prev_price[symbol] = price
-        if symbol not in self.__dq:
-            self.__dq[symbol] = deque()
+            self.__total_gain[symbol] = self.__total_gain[symbol] + price_change
         self.__dq[symbol].append(price_change)
+        
+        # if our deque doesn't have enough price changes, hold until we do
         if len(self.__dq.get(symbol, deque([]))) < 14:
             return ["HOLD"]
-        # remove from deque if it's over 14 observations
-        if self.__num_gain.get(symbol, 0) == 0 or self.__num_loss.get(symbol, 0) == 0:
-            return ["HOLD"]
-        avg_gain = self.__total_gain.get(symbol, 0.0) / self.__num_gain.get(symbol, 0)
-        avg_loss = self.__total_loss.get(symbol, 0.0) / self.__num_loss.get(symbol, 0)
-        rel_strength = avg_gain / avg_loss
-        rsi = 100 - (1 / (1 + rel_strength))
-        dq = self.__dq.get(symbol, deque([]))
-        while len(dq) > 14:
+
+        # trim while you can
+        dq = self.__dq[symbol]
+        
+        # we only exceed ideal deque length by 1 at any given point. we subtract value from loss/gain
+        if len(dq) > 14:
             old_change = dq.popleft()
             if old_change < 0:
-                self.__total_loss[symbol]  = self.__total_loss.get(symbol, 0.0) - abs(old_change)
-                self.__num_loss[symbol]  = self.__num_loss.get(symbol, 0) - 1
+                self.__total_loss[symbol]  = self.__total_loss[symbol] - abs(old_change)
             elif old_change > 0:
-                self.__total_gain[symbol]  = self.__total_gain.get(symbol, 0.0) - old_change
-                self.__num_gain[symbol]  = self.__num_gain.get(symbol, 0) - 1
+                self.__total_gain[symbol]  = self.__total_gain[symbol] - old_change
+        
+        # get average gain/loss for rsi
+        avg_gain = self.__total_gain[symbol] / 14
+        avg_loss = self.__total_loss[symbol] / 14
+        
+        # make sure average gain/loss isn't maximum/minimum RSI values.
+        if avg_gain == 0:
+            rsi = 0
+        elif avg_loss == 0:
+            rsi = 100
+        else:
+            rel_strength = avg_gain / avg_loss
+            rsi = 100 - (100 / (1 + rel_strength))
+        
+        # use RSI to determine signaling
         if rsi < 30:
             return ["BUY"]
         elif rsi > 70:
--- Assignment2/volatility_breakout_strategy.py
+++ Assignment2/volatility_breakout_strategy.py
@@ -4,9 +4,7 @@ from collections import deque
 
 class VolatilityBreakoutStrategy(Strategy):
     def __init__(self, window=20):
-        # TODO: Intialize
         self.name = "volatility"
-        self.__n = {}
         self.__prev_returns = {}
         self.__prev_price = {}
         self.__window = window
@@ -14,26 +12,33 @@ class VolatilityBreakoutStrategy(Strategy):
 
     def generate_signals(self, tick):
         price, symbol = tick.price, tick.symbol
+
+        # initialize symbol if not already initialized
         if symbol not in self.__prev_returns:
             self.__prev_returns[symbol] = deque(maxlen=self.__window)
             self.__prev_price[symbol] = price
             return ["HOLD"]
+
         # add new value
-        prev_price = self.__prev_price.get(symbol, 0.0)
-        daily_return = (price - prev_price) / prev_price
-        # add newest value
-        self.__prev_returns[symbol].append(daily_return)
-        # add price to deque after computing volatility.
-        self.__prev_price[symbol] = price
-        if len(self.__prev_returns[symbol]) < self.__window:
-            self.__prev_returns[symbol].append(daily_return)
+        prev_price = self.__prev_price[symbol]
+        if prev_price == 0.0:
             return ["HOLD"]
-        # TODO:  compute rolling 20-day volatility
-        past_vol = stat.stdev(self.__prev_returns[symbol])
+
+        # get daily return on prices, and update previous returns/prices
+        daily_return = (price - prev_price) / prev_price
+        self.__prev_returns[symbol].append(daily_return)
+        self.__prev_price[symbol] = price
+
+        if len(self.__prev_returns[symbol]) < self.__window:
+            return ["HOLD"]
+        # compute population standard deviation from current returns
+        past_vol = stat.pstdev(self.__prev_returns[symbol])
         # TODO: signal buy if daily return is greater than rolling 20-day volatility
         if past_vol < daily_return:
             return ["BUY"]
         # elif past_vol > daily_return:
         #     return ["SELL"]
+        elif daily_return < -past_vol:
+            return ["SELL"]
         else:
             return ["HOLD"]
