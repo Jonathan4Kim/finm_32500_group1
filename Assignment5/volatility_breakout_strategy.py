ed5ed8cacda85752701475c7ff1d00c2b7fbe804
Jonathan Kim <112917210+Jonathan4Kim@users.noreply.github.com>
2025-10-13 11:54:35 -0500

    Initialization

---
+++ Assignment5/volatility_breakout_strategy.py
@@ -0,0 +1,42 @@
+from strategies import Strategy
+import numpy as np
+from collections import deque
+
+class VolatilityBreakoutStrategy(Strategy):
+    def __init__(self, window=20):
+        self.name = "volatility"
+        self.__prev_returns = {}
+        self.__prev_price = {}
+        self.__window = window
+
+    def generate_signals(self, tick):
+        price, symbol = tick.price, tick.symbol
+
+        # initialize symbol if not already initialized
+        if symbol not in self.__prev_returns:
+            self.__prev_returns[symbol] = deque(maxlen=self.__window)
+            self.__prev_price[symbol] = price
+            return ["HOLD"]
+
+        prev_price = self.__prev_price[symbol]
+        if prev_price == 0.0:
+            return ["HOLD"]
+
+        # compute daily return
+        daily_return = (price - prev_price) / prev_price
+        self.__prev_returns[symbol].append(daily_return)
+        self.__prev_price[symbol] = price
+
+        # not enough history yet
+        if len(self.__prev_returns[symbol]) < self.__window:
+            return ["HOLD"]
+
+        # rolling population volatility
+        past_vol = np.std(self.__prev_returns[symbol], ddof=0)
+
+        if daily_return > past_vol:
+            return ["BUY"]
+        elif daily_return < -past_vol:
+            return ["SELL"]
+        else:
+            return ["HOLD"]
