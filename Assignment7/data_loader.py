c4178c353196368115ced024d71f831510d5fed7
Cole <colekoryto17@gmail.com>
2025-10-30 18:55:23 -0500

    Assignment 7 rolling stds

--- Assignment7/data_loader.py
+++ Assignment7/data_loader.py
@@ -4,11 +4,12 @@ import polars as pl
 from memory_profiler import profile
 
 def load_data_pandas():
-    return pd.read_csv("market_data-1.csv", parse_dates=["timestamp"])
+    return pd.read_csv("market_data-1.csv", parse_dates=["timestamp"]).sort_values(["symbol", "timestamp"], ignore_index=True)
 
 
 def load_data_polars():
-    return pl.read_csv("market_data-1.csv", try_parse_dates=True)
+    return pl.read_csv("market_data-1.csv", try_parse_dates=True).sort(["symbol", "timestamp"])
+
 
 # This function is added because adding profile decorator adds overhead that impacts execution speed
 @profile
--- Assignment7/metrics.py
+++ Assignment7/metrics.py
@@ -3,15 +3,12 @@ from data_loader import load_data_pandas, load_data_polars
 import polars as pl
 
 
-def add_rolling_mean_pandas():
-    # return load_data_pandas().groupby("symbol").rolling(20)["price"].mean()
-    df = load_data_pandas()
+def add_rolling_mean_pandas(df = load_data_pandas()):
     df["rolling_mean_20"] = df.groupby("symbol").rolling(20)["price"].mean().reset_index(drop=True)
     return df
 
 
-def add_rolling_mean_polars():
-    df = load_data_polars()
+def add_rolling_mean_polars(df = load_data_polars()):
     df = df.with_columns(
         pl.col("price")
         .rolling_mean(window_size=20)
@@ -21,6 +18,30 @@ def add_rolling_mean_polars():
     return df
 
 
+def add_rolling_std_pandas(df = load_data_pandas()):
+    df["rolling_std_20"] = (
+        df.groupby("symbol")["price"]
+        .rolling(20)
+        .std()
+        .reset_index(level=0, drop=True)
+    )
+    return df
+
+
+def add_rolling_std_polars(df = load_data_polars()):
+    df = df.with_columns(
+            pl.col("price")
+            .rolling_std(window_size=20)
+            .over("symbol")
+            .alias("rolling_std_20")
+        )
+    return df
+
+
 if __name__ == "__main__":
+    print("***Means***")
     print(f"Pandas rolling average time = {timeit.timeit(add_rolling_mean_pandas, number=1)}")
     print(f"Polars rolling average time = {timeit.timeit(add_rolling_mean_polars, number=1)}")
+    print("***Stds***")
+    print(f"Pandas rolling stds time = {timeit.timeit(add_rolling_std_pandas, number=1)}")
+    print(f"Polars rolling stds time = {timeit.timeit(add_rolling_std_polars, number=1)}")
