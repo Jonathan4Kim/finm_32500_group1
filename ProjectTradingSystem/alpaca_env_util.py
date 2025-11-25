f96af1de86082976f662bb8b4f5022745d2799ec
Adith Srinivasan <adith.srinivasan@gmail.com>
2025-11-25 00:22:37 -0600

    alpaca api utility

--- ProjectTradingSystem/alpaca_env_util.py
+++ ProjectTradingSystem/alpaca_env_util.py
@@ -2,7 +2,7 @@ import os
 import re
 from alpaca.trading.client import TradingClient
 
-def load_key(path):
+def read_key(path):
     with open(path, "r") as f:
         return f.read().strip()
     
@@ -12,12 +12,18 @@ class Secret(str):
         """Replace actual string with * when printing to the screen."""
         return re.sub(r"\S", "*", self)
 
-if __name__ == "__main__":
-    api_key = load_key(".alpaca_trading_api_key")
-    api_secret = load_key(".alpaca_trading_api_secret")
+def load_keys():
+    api_key = read_key(".alpaca_trading_api_key")
+    api_secret = read_key(".alpaca_trading_api_secret")
 
     print(f"Your key was read: {Secret(api_key)}")
     print(f"Your secret was read: {Secret(api_secret)}")
+
+    return api_key, api_secret
+
+if __name__ == "__main__":
+
+    api_key, api_secret = load_keys()
 
     try:
         trading_client = TradingClient(api_key, api_secret, paper=True)
